dee44c1c3a9c0f745d5fb5b7331a6c98d206305c
Alex Clark <aclark@aclark.net>
2018-02-19 13:39:46 -0500

    Update

--- project/app/tests.py
+++ project/app/tests.py
@@ -1,5 +1,7 @@
+from django.test import Client
 from django.test import TestCase
 from .models import Animal
+import unittest
 
 # Create your tests here.
 
@@ -15,3 +17,19 @@ class AnimalTestCase(TestCase):
         cat = Animal.objects.get(name="cat")
         self.assertEqual(lion.speak(), 'The lion says "roar"')
         self.assertEqual(cat.speak(), 'The cat says "meow"')
+
+
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get('/customer/details/')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the rendered context contains 5 customers.
+        self.assertEqual(len(response.context['customers']), 5)
