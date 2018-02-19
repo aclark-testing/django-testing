3bceb7cefd5ae7befe7141d1660babd8aab8388b
Alex Clark <aclark@aclark.net>
2018-02-19 10:55:53 -0500

    Update

--- project/app/models.py
+++ project/app/models.py
@@ -8,3 +8,6 @@ class Animal(models.Model):
 
     name = models.CharField(max_length=8, default='lion')
     sound = models.CharField(max_length=8, default='roar')
+
+    def speak(self):
+        print("The %s says %s." % (self.name, self.sound))
